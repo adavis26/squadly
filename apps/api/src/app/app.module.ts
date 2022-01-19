6689cc91152751c50b68463d6b310ed71e9ed077
davisadj <davisadj@iu.edu>
2022-01-19 00:18:14 -0600

    add logger

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,9 +3,28 @@ import { UsersModule } from './modules/users/users.module';
 import { Module } from '@nestjs/common';
 import { ChatModule } from './modules/chat/chat.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [AuthModule, UsersModule, ChatModule, DatabaseModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ChatModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: '../../../../.env'
+    }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        prettyPrint: {
+          colorize: true,
+          levelFirst: true,
+          ignore: 'req.headers,req.remoteAddress,req.remotePort',
+        },
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
