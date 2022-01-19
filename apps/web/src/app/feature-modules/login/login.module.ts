832499085c33be6d22335b07ce4bd81a01e90c22
davisadj <davisadj@iu.edu>
2022-01-18 21:19:53 -0600

    btn to signup

--- apps/web/src/app/feature-modules/login/login.module.ts
+++ apps/web/src/app/feature-modules/login/login.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { SharedModule } from 'apps/web/src/shared/shared.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, SignUpComponent]
 })
 export class LoginModule { }
